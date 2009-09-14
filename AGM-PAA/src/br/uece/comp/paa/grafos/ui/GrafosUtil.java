25b814bb969ab3581545c72d36d58027acee7359
fabiano.bie <fabiano.bie@40326ac2-9766-11de-9b84-8723755ecde9>
2009-09-14 22:23:48 +0000

    Desenha Grafico e ler arquivos testado e funcionando

--- AGM-PAA/src/br/uece/comp/paa/grafos/ui/GrafosUtil.java
+++ AGM-PAA/src/br/uece/comp/paa/grafos/ui/GrafosUtil.java
@@ -11,13 +11,19 @@ import java.awt.Color;
 import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.BorderFactory;
 
 import org.jgraph.JGraph;
+import org.jgraph.graph.DefaultCellViewFactory;
+import org.jgraph.graph.DefaultEdge;
 import org.jgraph.graph.DefaultGraphCell;
+import org.jgraph.graph.DefaultGraphModel;
 import org.jgraph.graph.GraphConstants;
+import org.jgraph.graph.GraphLayoutCache;
+import org.jgraph.graph.GraphModel;
 
 import br.uece.comp.paa.grafos.Aresta;
 import br.uece.comp.paa.grafos.Grafo;
@@ -41,7 +47,56 @@ public class GrafosUtil{
 	}
 	
 	public static JGraph desenhaGrafo(Grafo<String> G){
-		return null;
+		GraphModel model = new DefaultGraphModel();
+		GraphLayoutCache view = new GraphLayoutCache(model,new DefaultCellViewFactory());
+		JGraph graph = new JGraph(model, view);
+
+		ArrayList<Vertice<String>> vrtxs = G.getVertices();
+		DefaultGraphCell[] cells = new DefaultGraphCell[vrtxs.size()];
+
+		for(int i=0; i< vrtxs.size();i++){
+			cells[i] = GrafosUtil.createCell(vrtxs.get(i).getInfo(),
+					new Rectangle2D.Double(vrtxs.get(i).getPosX(),vrtxs.get(i).getPosY(), 20, 20), false);
+		}
+		
+		vrtxs = new ArrayList<Vertice<String>>();
+		int i=0;
+		for (Vertice<String> V : G.getVertices()){
+			vrtxs.add(V);
+			for (Aresta<String> E : V.getListAdj()) {
+				boolean a,b;
+				a=vrtxs.contains(E.getA());
+				b=vrtxs.contains(E.getB());
+				if (!(a && b) || (!a && b) || (a && !b)) {
+					DefaultEdge edge = new DefaultEdge(E.getPeso());
+					
+					i =  searchIdCell(E.getA(),cells);
+					edge.setSource(cells[i].getChildAt(0));
+					
+					i =  searchIdCell(E.getB(),cells);
+					edge.setTarget(cells[i].getChildAt(0));
+					
+					view.setAutoSizeOnValueChange(false);
+					int arrow = GraphConstants.ARROW_NONE;
+					GraphConstants.setLineEnd(edge.getAttributes(), arrow);
+					GraphConstants.setEndFill(edge.getAttributes(), true);
+					
+					graph.getGraphLayoutCache().insert(edge);	
+					System.out.println(E.getA()+"----"+E.getB());
+				}
+			}
+		}
+		
+				graph.getGraphLayoutCache().insert(cells);
+		
+		return graph;
+	}
+	
+	private static int searchIdCell(Vertice<String> V, DefaultGraphCell[] cells){
+		for (int i=0 ; i< cells.length; i++) {
+			if(cells[i].toString().equals(V.getInfo())) return i;
+		}
+		return -1;
 	}
 	
 	public static Grafo<String> fileToGrafo(String nameFile) throws FileNotFoundException{
@@ -63,17 +118,12 @@ public class GrafosUtil{
 			}
 			for (int i = 0; i < numAresta; i++) {
 				Aresta<String> edg = new Aresta<String>(new Vertice<String>(scan.next()),new Vertice<String>(scan.next()),Double.parseDouble(scan.next()));
+				G.addElem(edg);
 			}
 		}else if(tipo==0){
-			
+			System.out.println("Erro de Formato: leitura ainda nao implementada");
 		}
-		
-		
-		
-		
-		
-		
-		return null;
+		return G;
 	}
 	
 	public static Double distancia(Vertice<String> V1 ,Vertice<String> V2) {
