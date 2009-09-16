ae4a66875d43eff796fbc1988e942d097d7161cc
fabiano.bie <fabiano.bie@40326ac2-9766-11de-9b84-8723755ecde9>
2009-09-16 12:24:07 +0000

    interface para os algoritmo AGM

---
+++ trunk/AGM-PAA/src/br/uece/comp/paa/agm/interfaces/Iagm.java
@@ -0,0 +1,24 @@
+/**
+ * PAA - Projeto de Analise e Algoritmos
+ * Universidade Estadual do Ceará - UECE
+ * Alunos: Fabiano Tavares e Diego Sá
+ * Prof. Marcos Negreiro
+ *
+ */
+package br.uece.comp.paa.agm.interfaces;
+
+import java.util.ArrayList;
+
+import br.uece.comp.paa.grafos.Aresta;
+import br.uece.comp.paa.grafos.Grafo;
+import br.uece.comp.paa.grafos.Vertice;
+
+/**
+ * @author Fabiano Tavares (fabiano.bie@gmail.com)
+ *
+ */
+public interface Iagm <T>{
+	
+	public Grafo<T> obterAGM(Grafo<T> grafo) throws CloneNotSupportedException;
+	
+}
